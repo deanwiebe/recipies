65313ae70429304a855058fc021a86aacf868824
deanwiebe <wiebedean@gmail.com>
2020-03-18 15:33:48 -0600

    working on header. changes at header.php and style.css

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 <?php require 'templates/header.php'?>
 
 <div class="main">
-    <h2>this is the body</h2>
-    <h2>this is the body again</h2>
 </div>
     
 <?php require 'templates/footer.php'?>
